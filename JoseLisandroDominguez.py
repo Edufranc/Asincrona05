c56e79e6ae95040014b653f58aa0160a6ba7153d
JLD986 <126208860+JLD986@users.noreply.github.com>
2023-02-23 17:05:32 -0600

    Add files via upload

---
+++ JoseLisandroDominguez.py
@@ -0,0 +1,9 @@
+edad=19
+nombre= " Jose Lisandro "
+apellido=" Ramos Dominguez "
+sexo= " masculino "
+edadfinal= edad+5
+
+print(f"{nombre}, {apellido}, {sexo},  {edad}")
+
+print("La edad del alumno al terminar la carrera sera de ", edadfinal)
